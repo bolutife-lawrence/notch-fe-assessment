9e63a194f6376a0455e8c41e58be1a04e12ccaa9
beelaw <lawrence.mcbhee@gmail.com>
2021-06-27 14:39:49 +0100

    Adds Notch favicon and removes more unnecessary files.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
 
-import * as serviceWorker from './serviceWorker';
-
 import "./scss/styles.scss";
 
 ReactDOM.render(
@@ -17,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
